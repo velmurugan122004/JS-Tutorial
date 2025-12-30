d6daeccbef24a05178ced1b1fe62c1111dbc80e8
unknown <velmurugan12012004@gmail.com>
2025-12-30 07:05:58 +0530

    to-do-list javascript program

--- Scripts-folder/9.1-to-do-list.js
+++ Scripts-folder/9.1-to-do-list.js
@@ -1,5 +1,7 @@
 const todolist=[];//empty array
 
+const list=[];
+looptodisplay();
 function addtodo()
 {
   const inputElement=document.querySelector('.js-name-input1');
@@ -12,5 +14,29 @@ function addtodo()
 
   console.log(todolist);
 
-  inputElement.value='';
+  inputElement.value='';//after enter input label to free
 }
+function looptodisplay()
+{
+  let todolistHTML='';
+
+  for(let i=0;i<list.length;i++)
+  {
+    const todo=list[i];
+    const html=`<p>${todo}</p>`; //generating html uing javascript
+    todolistHTML+=html;
+  }
+  console.log(todolistHTML);
+
+  document.querySelector('.js-display-output').innerHTML=todolistHTML;
+}
+function addpractice()
+{
+  const input=document.querySelector('.js-name-input2');
+  list.push(input.value);
+  console.log(list);
+
+  input.value='';
+  looptodisplay();
+}
+
