07f5a57d9f647cce601ecf98d085e866fbfd7899
velmurugan122004 <velmurugan12012004@gmail.com>
2026-01-29 19:17:48 +0530

    upadte somethings on my website

--- javascript-amazon-project-main/Scripts/amazon.js
+++ javascript-amazon-project-main/Scripts/amazon.js
@@ -1,36 +1,3 @@
-const products=[{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating:{
-    star:4.5,
-    count:87
-  },
-  price:1090
-},{
-  image:'images/products/intermediate-composite-basketball.jpg',
-  name:'Intermediate Size Basketball',
-  rating:{
-    star:4,
-    count:127
-  },
-  price:2095
-},{
-  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    star:4.5,
-    count:56
-  },
-  price:799
-},{
-  image:'images/products/black-2-slot-toaster.jpg',
-  name:'2 Slot Toaster-Black',
-  rating:{
-    star:5,
-    count:2197
-  },
-  price:1899
-}];
 
 let productsHTML='';
 
@@ -48,14 +15,14 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star *10}.png">
+              src="images/ratings/rating-${product.rating.stars *10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.price/100).toFixed(2)}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -80,12 +47,44 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-to-add-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
     `;
 });
-console.log(productsHTML);
+//console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+
+document.querySelectorAll('.js-to-add-cart')
+    .forEach((button)=>{
+      button.addEventListener('click',()=>{
+        console.log('Added product');
+        //console.log(button.dataset.productName);
+        const productId=button.dataset.productId;
+
+        //checking item in cart
+        let matchingItem;
+        cart.forEach((item)=>{
+          if(productId===item.productId)
+          {
+            matchingItem=item;
+          }
+        });
+
+        //if already there means increase cart quantity
+        if(matchingItem){
+          matchingItem.quantity+=1;
+        }
+        else{
+          cart.push({
+          productId:productId,
+          quantity:1
+        });
+        }
+
+        
+        console.log(cart);
+      });
+});
