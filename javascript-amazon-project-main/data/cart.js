ed83fc0efbbacf3e155c29b6cd763de821268811
velmurugan122004 <velmurugan12012004@gmail.com>
2026-02-04 12:07:09 +0530

    update of checkout page in some predefined content

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,10 @@
-export const cart=[];
+export const cart=[{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2
+},{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb68452', 
+  quantity:1
+}];
 
 export function addToCart(productId){
     //checking item in cart
