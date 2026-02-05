7b1b2d0b77b582458e23e4f0bd178dbe46e7072d
velmurugan122004 <velmurugan12012004@gmail.com>
2026-02-05 10:43:37 +0530

    delete cart item in cart array and checkout page also

--- javascript-amazon-project-main/Scripts/checkout.js
+++ javascript-amazon-project-main/Scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
     }); console.log(matchingProduct);
     
     cartSummary+=`
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -101,5 +101,7 @@ document.querySelectorAll('.js-delete-cart').forEach((link)=>{
         console.log(productId);
         removeProduct(productId) ;
         console.log(cart);
+        const container=document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
