f66075f121756d9d9b622817116f8089686690f7
velmurugan122004 <velmurugan12012004@gmail.com>
2026-02-28 07:49:17 +0530

    create class version of cart

--- javascript-amazon-project-main/Scripts/checkout.js
+++ javascript-amazon-project-main/Scripts/checkout.js
@@ -4,7 +4,7 @@ import { orderSummary } from "./checkout/orderSummary.js";
 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 orderSummary();
---
+++ javascript-amazon-project-main/data/cart-class.js
@@ -0,0 +1,121 @@
+class Cart{
+  cartItems;// this form to add properties
+
+  localStorageKey;
+
+  constructor(localStorageKey){
+    this.localStorageKey=localStorageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage(){
+    this.cartItems =JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if(!this.cartItems){
+      this.cartItems=[{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2,
+        deliveryOptionId:'1'
+      },{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb68452', 
+        quantity:1,
+        deliveryOptionId:'2'
+      }];
+    }
+  };
+
+  saveStorage()
+  {
+    localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+  };
+
+  addToCart(productId,userQuantity){
+    //checking item in cart
+        let matchingItem;
+        userQuantity = Number(userQuantity);
+        this.cartItems.forEach((cartItem)=>{
+          if(productId===cartItem.productId)
+          {
+            matchingItem=cartItem;
+          }
+        });
+
+        //if already there means increase cart quantity
+        
+        if(matchingItem){
+          matchingItem.quantity+= userQuantity;
+        }
+        else{
+          this.cartItems.push({
+              productId:productId,
+              quantity:userQuantity,
+              deliveryOptionId:'1'
+          });
+        }
+      this.saveStorage();
+
+  };
+
+  removeProduct(productId)
+  {
+      const newCart=[];
+
+      this.cartItems.forEach((cartItem)=>{
+          if(cartItem.productId!==productId){
+            newCart.push(cartItem);
+          }
+      });
+
+      this.cartItems=newCart;
+      this.saveStorage();
+  };
+
+  UpdateDeliveryOption(productId,userSelectDelivery)
+  {
+    let matchingItem;
+    this.cartItems.forEach((cartItem)=>{
+        if(productId===cartItem.productId)
+        {
+          matchingItem=cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId=userSelectDelivery;
+    this.saveStorage();
+  };
+
+  calculateCartQuantity()
+  {
+        let cartQuantity=0;
+          
+        this.cartItems.forEach((cartItem)=>{
+          cartQuantity+=cartItem.quantity;
+        });
+          
+        //console.log(cartQuantity);
+          //console.log(cart);
+        return cartQuantity;
+        
+  };
+
+  updateCartQuantity(newQuantity,productId)
+  {
+      this.cartItems.forEach((cartItem)=>{
+          if(cartItem.productId===productId)
+          {
+            cartItem.quantity=Number(newQuantity);
+          }
+      });
+      this.saveStorage();
+  }
+}
+
+
+
+const cart=new Cart('cart-oop');
+console.log(cart);
+
+const businessCart=new Cart('cart-business');
+console.log(businessCart);
+
+console.log(businessCart instanceof Cart);//it can check class instance of businessCart
